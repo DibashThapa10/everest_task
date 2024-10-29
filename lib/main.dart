96dc3d4ac2b5691a7f72347de9b0d51e7577a2ca
Dibash Thapa <dibashthapa447@gmail.com>
2024-10-29 16:32:40 +0545

    create splash screen and implement go router package for navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,25 @@
 import 'package:everest_task/view/post_view.dart';
+import 'package:everest_task/view/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
+final GoRouter _router = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const SplashView(),
+    ),
+    GoRoute(
+      path: '/post_view',
+      builder: (context, state) => const PostView(),
+    ),
+  ],
+);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -12,14 +27,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ProviderScope(
-      child: MaterialApp(
+      child: MaterialApp.router(
         debugShowCheckedModeBanner: false,
         title: 'Task App',
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: const PostView(),
+        routerConfig: _router,
       ),
     );
   }
